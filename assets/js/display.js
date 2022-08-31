f24bc59a71517aac4d9665b9655b97306d46ad3e
JonWong2020 <jonathannwong50@aol.com>
2022-08-31 14:04:44 -0700

    got data for show api

--- assets/js/display.js
+++ assets/js/display.js
@@ -7,13 +7,20 @@ function getParams() {
   var query = searchParamsArr.split("=").pop();
   // console.log(query)
   artistSearch(query);
+  showSearch(query);
 }
 
-function printResults(resultObj) {
+function printVideoResults(resultObj) {
+  var randomIndex = Math.floor(Math.random()*resultObj.length);
+  console.log(resultObj[randomIndex]);
   var youtubeEl = document.querySelector("#youtube-video");
-  var youtubeUrl = "https://youtube.com/embed/" + resultObj[Math.floor(Math.random()*resultObj.length)].id
+  var youtubeUrl = "https://youtube.com/embed/" + resultObj[randomIndex].id
   console.log(youtubeUrl);
   youtubeEl.setAttribute("src", youtubeUrl);
+}
+
+function printShowResults(band) {
+
 }
 
 function artistSearch(query) {
@@ -30,10 +37,24 @@ function artistSearch(query) {
   fetch(ytUrl, options)
     .then((response) => response.json())
     .then((response) => {
-      printResults(response.result.videos)
+      printVideoResults(response.result.videos)
     })
     .catch((err) => console.error(err));
+
+    
 };
+function showSearch(query) {
+  var url = "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=AV9mg9h1UJljjM1Ap3l1q17CLarnAboN&keyword=" + query;
+  fetch(url)
+  .then((response) => response.json())
+  .then(data => {
+    var eventUrl = "https://app.ticketmaster.com/discovery/v2/events/"+ data._embedded.events[0].id +"?size=1&apikey=AV9mg9h1UJljjM1Ap3l1q17CLarnAboN"
+    console.log(data._embedded.events[0].id)
+    fetch(eventUrl)
+    .then((response2) => response2.json())
+    .then(data2 => console.log(data2))
+  })
+}
 
 getParams();
 // add event listener to the search button
